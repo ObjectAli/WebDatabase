b494e6486664d3338942683ff36334fc9db5be2c
Ali <alinorbobaev97@mail.ru>
2019-02-27 20:43:34 +0300

    start-project

---
+++ src/test/java/com/moodle/sevsu/webdb/WebdbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.moodle.sevsu.webdb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebdbApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
