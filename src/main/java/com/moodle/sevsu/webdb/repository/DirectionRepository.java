a832f225db89c5ef7598d5db78d31b270bed1698
Ali <alinorbobaev97@mail.ru>
2019-03-19 12:10:41 +0300

    crud-completed

--- src/main/java/com/moodle/sevsu/webdb/repository/DirectionRepository.java
+++ src/main/java/com/moodle/sevsu/webdb/repository/DirectionRepository.java
@@ -3,5 +3,6 @@ package com.moodle.sevsu.webdb.repository;
 import com.moodle.sevsu.webdb.entity.Direction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface DirectionRepository extends JpaRepository<Direction, Integer> {
 }
